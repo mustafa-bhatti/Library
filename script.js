e0962b07d5a18bb482e5a7ba78d727be4c2144a9
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-06-15 21:32:30 +0500

    create functions to add book object in the array

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 // Library
+
+const myLibray = [];
+
+function book(name,author,pages,read){
+    /* Expected values
+    book(Hobbit, J.R.R Tolkein, 287, true )     */
+
+    this.id = crypto.randomUUID();
+    this.name = name;
+    this.author = author;
+    this.page = pages;
+    this.read = read; 
+} 
+
+function addBookToLibrary(name,author,pages,read){
+    const newBook = new book(name,author,pages,read) 
+    myLibray.push(newBook)
+}
+addBookToLibrary("Hobbit", "J.R.R Tolkein", 287, "true")
+addBookToLibrary("Hobbit", "J.R.R Tolkein", 287, "true")
+addBookToLibrary("Hobbit", "J.R.R Tolkein", 287, "true")
+
+function displayBooks(){
+    for (let i=0; i<myLibray.length;i++){
+        
+    }
+}
+
+console.log(myLibray)
+
